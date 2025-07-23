86bbb475fc04019803f04e7c41561cf57cb012ee
Primocar <primoweb.ca@gmail.com>
2025-07-23 16:21:41 -0400

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1753302101557

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_30d5b2ec._.js",
+  "static/chunks/src_a7ddd62d._.js"
+],
+    source: "dynamic"
+});
